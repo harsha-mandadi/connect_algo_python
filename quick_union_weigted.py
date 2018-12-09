9073f6a40eb123b8373d461f95a25bfa01bf7e1b
harsha-mandadi <39717084+harsha-mandadi@users.noreply.github.com>
2018-12-09 10:52:56 -0500

    Add files via upload

---
+++ quick_union_weigted.py
@@ -0,0 +1,30 @@
+class quick_union_weighted(object):
+    '''dyamic connectivity with logN complexity'''
+    def __init__(self,N):
+        self.id = list(range(N))
+        self.size = [1 for i in range(N)]
+    def find_root(self,p):
+        while self.id[p] != p:
+            p = self.id[p]
+        return p
+    def union_two_points(self,p,q):
+        p_root = self.find_root(p)
+        q_root = self.find_root(q)
+        if(p_root == q_root):
+            return
+        elif(self.size[p_root]>=self.size[q_root]):
+            self.id[q_root] = p_root
+            self.size[q_root] += self.size[p_root]
+        elif(size[p]<size[q]):
+            self.id[p_root] = q_root
+            self.size[p_root] += self.size[q_root]
+    def find_if_connected(self,p,q):
+        return self.find_root(p) == self.find_root(q)
+connect = quick_union_weighted(10)
+connect.union_two_points(2,5)
+connect.union_two_points(7,3)
+connect.union_two_points(4,3)
+connect.union_two_points(2,3)
+print(connect.find_if_connected(7,4))
+
+
